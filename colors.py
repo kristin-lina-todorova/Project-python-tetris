7f78ef22215abefba5d37de7704c1852a2f1f4ad
krisi <125066650+kristin-lina-todorova@users.noreply.github.com>
2024-06-01 22:09:05 +0300

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,19 @@
+class Colors:
+    white = (0, 0, 0)
+    yellow = (255, 255, 0)
+    orange = (255, 165, 0)
+    red = (255, 0, 0)
+    pink = (255, 192, 203)
+    purple = (160, 32, 240)
+    light_blue = (175, 216, 230)
+    dark_blue = (0, 0, 139)
+    light_green = (144, 238, 144)
+    dark_green = (1, 50, 32)
+    black = (255, 255, 255)
+    background_color = (159, 193, 252)
+    grid_color = (3, 56, 148)
+    score_button_color = (78, 101, 166)
+    side_panel_color = (186, 196, 222)
+
+    def get_color(self):
+        return[self.white, self.yellow, self.orange, self.red, self.pink, self.purple, self.light_blue, self.dark_blue, self.light_green, self.dark_green, self.background_color, self.grid_color, self.score_button_color, self.side_panel_color]
